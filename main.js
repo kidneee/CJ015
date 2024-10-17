7be34ad9d39d04685cf7c519fa0cd252aa632325
kido <skido627@gmail.com>
2024-10-17 10:08:15 +0900

    10/15分

--- main.js
+++ main.js
@@ -1,13 +1,27 @@
 // ハンバーガーメニュー
-document.querySelector('.header-menu__btn').addEventListener('click', function () {
-  document.body.classList.toggle('menu-open');
+const hamburgerButton = document.querySelector('#js-buttonHamburger');
+
+hamburgerButton.addEventListener('click', (e) => {
+  const isExpanded = e.currentTarget.getAttribute('aria-expanded') !== 'false';
+  e.currentTarget.setAttribute('aria-expanded', !isExpanded);
+
+  document.documentElement.classList.toggle('is-drawerActive');
+});
+
+document.getElementById('js-buttonHamburger').addEventListener('click', function () {
+  const drawer = document.getElementById('drawerMenu');
+  if (drawer.classList.contains('open')) {
+    drawer.classList.remove('open');
+  } else {
+    drawer.classList.add('open');
+  }
 });
 
 // スライドイン
 window.addEventListener('scroll', function () {
   const elementsLeft = document.querySelectorAll('.js-slidein-left');
   const elementsRight = document.querySelectorAll('.js-slidein-right');
-  const elementsZoom = document.querySelectorAll('.js-zoomin');
+  const elementsZoom = document.querySelectorAll('.js-zoom');
 
   elementsLeft.forEach((element) => {
     const rect = element.getBoundingClientRect();
@@ -17,6 +31,13 @@ window.addEventListener('scroll', function () {
   });
 
   elementsRight.forEach((element) => {
+    const rect = element.getBoundingClientRect();
+    if (rect.top < window.innerHeight && rect.bottom > 0) {
+      element.classList.add('active');
+    }
+  });
+
+  elementsZoom.forEach((element) => {
     const rect = element.getBoundingClientRect();
     if (rect.top < window.innerHeight && rect.bottom > 0) {
       element.classList.add('active');
